575c3c2162174a4ea7a21061afa93f4658648a41
abubakar528 <muhammadabubakar7526@gmail.com>
2023-02-18 13:18:17 +0500

    font family and styled compnents added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Pacifico } from "@next/font/google";
+import type { AppProps } from "next/app";
+
+const pacifico = Pacifico({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={pacifico.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
